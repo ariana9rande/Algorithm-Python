68a60fed08361c0c7ee11334f455a58a2da8308c
ariana9rande <51096442+ariana9rande@users.noreply.github.com>
2022-03-07 13:44:27 +0900

    Create 41.py

---
+++ 제주/41.py
@@ -0,0 +1,13 @@
+input_data = int(input())
+
+count = 0
+
+for i in range(1, input_data + 1):
+    if input_data % i == 0:
+        count += 1
+        if count > 2:
+            print('NO')
+            break
+
+if i == input_data and count == 2:
+    print('YES')
