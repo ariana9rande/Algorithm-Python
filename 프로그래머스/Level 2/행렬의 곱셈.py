f58fc5e4aec56ab611fe39bef6874da15fe0ee73
ariana9rande <51096442+ariana9rande@users.noreply.github.com>
2022-04-05 23:18:13 +0900

    220404 / 220405

--- 프로그래머스/Level 2/행렬의 곱셈.py
+++ 프로그래머스/Level 2/행렬의 곱셈.py
@@ -1,4 +1,5 @@
-def solution(arr1, arr2):
+#220404
+/*def solution(arr1, arr2):
     answer = [[] for _ in range(len(arr1))]
     for i in range(len(arr1)):
         n = -1
@@ -11,4 +12,15 @@ def solution(arr1, arr2):
                 else:
                     answer[i][n] += arr1[i][k] * arr2[k][j]
                 k += 1
+    return answer*/
+
+#220405
+def solution(arr1, arr2):
+    answer = [[] for _ in range(len(arr1))]
+    for i in range(len(arr1)):
+        for j in range(len(arr2[0])):
+            tmp = 0
+            for k in range(len(arr1[0])):
+                tmp += arr1[i][k] * arr2[k][j]
+            answer[i].append(tmp)
     return answer
